38e83343c8aa66d9edebe968fcba209b431d16ad
Connor Spear <Connor.Spear@AssistRX.com>
2021-11-09 10:11:24 -0500

    Code standards

--- BasicSynthesizer/Oscillator.cs
+++ BasicSynthesizer/Oscillator.cs
@@ -10,7 +10,7 @@ namespace BasicSynthesizerProject
     {
         public Oscillator()
         {
-            Point buttonPoint = new Point(10,15);
+            Point buttonPoint = new(10,15);
             this.Controls.Add(new Button()
             {
                 Name = "Sine",
